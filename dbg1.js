037ed4871ceb6a5967d1ea6cc89a23f925c01b07
xiatian <2813576925@qq.com>
2025-01-16 15:13:02 +0800

    u

---
+++ dbg1.js
@@ -0,0 +1,33 @@
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
+const config = require('./config');
+let globalBrowser = null;
+
+const launch = (isShow = true) => {
+    return new Promise(async (resolve) => {
+        let browser = await puppeteer.launch({
+            args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-blink-features=AutomationControlled"],
+            dumpio: false,
+            headless: isShow ? !isShow : !config.isShow,
+            executablePath: config.chromePath,
+            timeout: 0,
+            userDataDir: './userdata'
+        });
+        globalBrowser = browser;
+        const page = await browser.newPage();
+        await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
+        await page.setViewport({
+            width: 1024,
+            height: 768,
+        });
+        await page.setViewport({ width: 0, height: 0 });
+        resolve(page);
+    });
+}
+
+(async () => {
+    const listAddress = 'https://paypayfleamarket.yahoo.co.jp/my/purchase';
+    let page = await launch(true).catch();
+    await page.goto(listAddress, { waitUntil: "networkidle0" }).catch(() => { });
+})();
